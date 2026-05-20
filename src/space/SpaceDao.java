38bae3cab59b3cbff4a7158f27d761d6287f0021
KIMSUNGKWAN <khska44@gmail.com>
2026-05-20 12:30:21 +0900

    Add comment

--- src/space/SpaceDao.java
+++ src/space/SpaceDao.java
@@ -4,15 +4,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SpaceDao {
-
+		// 명세 2.1
+		// input : space_key, space_title, space_status, image_no
+		// output : -
+		// space_key, space_title : 유저입력
+		// space_status : 공개(Y)/비공개(N)
+		// image_no : 스페이스 이미지 번호
 	boolean CreateSpace(String space_key, String space_title, int space_order, String space_status, int image_no)
 			throws Exception {
 		boolean isCreated = false;
 		String driver = "oracle.jdbc.driver.OracleDriver";
 		String url = "jdbc:oracle:thin:@localhost:1521:xe";
-		String dbId = "proj1";
+		String dbId = "jira";
 		String dbPw = "1234";
 
 		Class.forName(driver);
@@ -34,12 +41,17 @@ public class SpaceDao {
 
 		return isCreated;
 	}
-
+		// 명세 2.2
+		// input : space_key, user_no
+		// output : -
+		// space_key : 지우려는 스페이스키
+		// user_no : 현재 유저번호
+		// user_role = '관리자' 인 경우만 삭제가능
 	boolean DeleteSpace(String space_key, int user_no) throws Exception { // user_no = current user_no
 		boolean isDeleted = false;
 		String driver = "oracle.jdbc.driver.OracleDriver";
 		String url = "jdbc:oracle:thin:@localhost:1521:xe";
-		String dbId = "proj1";
+		String dbId = "jira";
 		String dbPw = "1234";
 
 		Class.forName(driver);
@@ -60,22 +72,30 @@ public class SpaceDao {
 
 		return isDeleted;
 	}
-	
-	boolean UpdateSpace(String new_space_title, String new_space_key, int user_no) throws Exception {
+		// 명세 2.3
+		// input : space_key, new_space_title, new_space_key, user_no
+		// output : -
+		// space_key : 수정하려는 스페이스키
+		// new_space_key : 새로운 스페이스키
+		// new_space_title : 새로운 스페이스타이틀
+		// user_no : 현재 유저번호
+		// user_role = '관리자' 인 경우만 삭제가능
+	boolean UpdateSpace(String space_key, String new_space_title, String new_space_key, int user_no) throws Exception {
 		boolean isUpdated = false;
 		String driver = "oracle.jdbc.driver.OracleDriver";
 		String url = "jdbc:oracle:thin:@localhost:1521:xe";
-		String dbId = "proj1";
+		String dbId = "jira";
 		String dbPw = "1234";
 
 		Class.forName(driver);
 		Connection conn = DriverManager.getConnection(url, dbId, dbPw);
 
-		String sql = "UPDATE space SET space_title = ?, space_key = ? WHERE space_key IN (SELECT space_key FROM space_members WHERE user_role = '관리자' AND user_no = ?";
+		String sql = "UPDATE space SET space_title = ?, space_key = ? WHERE space_key IN (SELECT space_key FROM space_members WHERE user_role = '관리자' AND user_no = ?) AND space_key = ?";
 		PreparedStatement pstmt = conn.prepareStatement(sql);
 		pstmt.setString(1, new_space_title);
 		pstmt.setString(2, new_space_key);
 		pstmt.setInt(3, user_no);
+		pstmt.setString(4, space_key);
 		int result = pstmt.executeUpdate();
 
 		if (result > 0) {
@@ -87,12 +107,15 @@ public class SpaceDao {
 
 		return isUpdated;
 	}
-	
-	SpaceListDto ShowSpaceList(int user_no) throws Exception {
-		SpaceListDto dto = null;
+		// 명세 2.4
+		// input : user_no
+		// output : List<SpaceListDto>
+		// user_no : 현재 유저번호
+	List<SpaceListDto> ShowSpaceList(int user_no) throws Exception {
+		List<SpaceListDto> list = new ArrayList<>();
 		String driver = "oracle.jdbc.driver.OracleDriver";
 		String url = "jdbc:oracle:thin:@localhost:1521:xe";
-		String dbId = "proj1";
+		String dbId = "jira";
 		String dbPw = "1234";
 
 		Class.forName(driver);
@@ -105,23 +128,29 @@ public class SpaceDao {
 		ResultSet rs = pstmt.executeQuery();
 		
 		while(rs.next()) {
-			dto = new SpaceListDto();
-			dto.setSpaceKey(rs.getString("sm.space_key"));
+			SpaceListDto dto = new SpaceListDto();
+			dto.setSpaceKey(rs.getString("space_key"));
 			dto.setSpaceTitle(rs.getString("space_title"));
 			dto.setImageNo(rs.getInt("image_no"));
+			
+			list.add(dto);
 		}
+		
 		rs.close();
 		pstmt.close();
 		conn.close();
 		
-		return dto;
+		return list;
 	}
-	
+		// 명세 2.5
+		// input : space_key
+		// output : SpaceListDto(space_key, space_title, image_no)
+		// space_key : 현재 스페이스키
 	SpaceListDto ShowSpaceProfile(String space_key) throws Exception {
 		SpaceListDto dto = null;
 		String driver = "oracle.jdbc.driver.OracleDriver";
 		String url = "jdbc:oracle:thin:@localhost:1521:xe";
-		String dbId = "proj1";
+		String dbId = "jira";
 		String dbPw = "1234";
 
 		Class.forName(driver);
