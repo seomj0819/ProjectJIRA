a4f1fd21693b11f30d1418474a39bbe0573a502d
Minjae Seo <seomj081923@gmail.com>
2026-06-02 17:13:52 +0900

    Update UserProfileDto

--- src/UserProfileDto.java
+++ src/UserProfileDto.java
@@ -12,35 +12,19 @@ public class UserProfileDto {
 		this.imageTitle = imageTitle;
 	}
 	
-	public int getUserNo() {
-		return userNo;
-	}
-	public void setUserNo(int userNo) {
-		this.userNo = userNo;
-	}
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getImageTitle() {
-		return imageTitle;
-	}
-	public void setImageTitle(String imageTitle) {
-		this.imageTitle = imageTitle;
-	}
-	public UserProfileDto(){
-	}
+	public int getUserNo() {return userNo;}
+	public void setUserNo(int userNo) {this.userNo = userNo;}
+	public String getUserName() {return userName;}
+	public void setUserName(String userName) {this.userName = userName;}
+	public String getEmail() {return email;}
+	public void setEmail(String email) {this.email = email;}
+	public String getImageTitle() {return imageTitle;}
+	public void setImageTitle(String imageTitle) {this.imageTitle = imageTitle;}
+	public UserProfileDto(){}
+	
 	@Override
 	public String toString() {
-		return "UserProfileDto [userNo=" + userNo + ", userName=" + userName + ", email=" + email + ", imageTitle="
-				+ imageTitle + "]";
+		return "UserProfileDto [userNo = " + userNo + ", userName = " + userName + 
+				", email = " + email + ", imageTitle = " + imageTitle + "]";
 	}
 }
