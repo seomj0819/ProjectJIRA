09ae943faa481c80f3f42cbee1764a6e024ba4ca
Minjae Seo <seomj081923@gmail.com>
2026-06-05 17:56:25 +0900

    Update SpaceMembersDao

--- src/SpaceMembersDao.java
+++ src/SpaceMembersDao.java
@@ -17,69 +17,53 @@ public class SpaceMembersDao {
 	// 초대코드가 발송되었으면 true, 아니면 false
 	// 초대된 유저가 접속코드 인증시 초대완료 (Verification Code, Expire Date -> null)
 	boolean isSpaceMemberDuplicate (String spaceKey, String email, int currentUserNo) throws Exception{
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
 		boolean isDuplicate = false;
-		
-		Class.forName(driver);
-		conn = DriverManager.getConnection(url, dbId, dbPw);
-		
 		String sql = "SELECT * FROM space_members WHERE space_key = ? AND user_no = ?";
-		pstmt = conn.prepareStatement(sql);
-		pstmt.setString(1, spaceKey);
-		pstmt.setInt(2, currentUserNo);
-		rs = pstmt.executeQuery();
 		
-		if(rs.next()) {
-			isDuplicate = true;
+		try (Connection conn = DriverManager.getConnection(url, dbId, dbPw);
+			PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pstmt.setString(1, spaceKey);
+			pstmt.setInt(2, currentUserNo);
+
+			try (ResultSet rs = pstmt.executeQuery()) {
+				if(rs.next()) {
+					isDuplicate = true;
+				}
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
-		
-		rs.close();
-		pstmt.close();
-		conn.close();
-		
 		return isDuplicate;
 	}
 	
 	void createInviteCode(String spaceKey, int userNo, String userRole, int currentUserNo) throws Exception {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		PreparedStatement pstmt2 = null;
-		ResultSet rs = null;
+	    String checkSql = "SELECT 1 FROM space_members WHERE user_no = ? AND space_key = ?";
+	    String insertSql = "INSERT INTO space_members(space_key, user_no, user_role, invite_code, expire_date) "
+	                     + "VALUES (?, ?, ?, ?, SYSDATE + (1/24/60 * 10))";
 
-		Class.forName(driver);
-		conn = DriverManager.getConnection(url, dbId, dbPw);
+	    try (Connection conn = DriverManager.getConnection(url, dbId, dbPw);
+	         PreparedStatement checkPstmt = conn.prepareStatement(checkSql)) {
+	        
+	        checkPstmt.setInt(1, currentUserNo);
+	        checkPstmt.setString(2, spaceKey);
+	        
+	        try (ResultSet rs = checkPstmt.executeQuery()) {
+	            if (!rs.next()) {
+	                throw new IllegalAccessException("초대 권한이 없습니다.");
+	            }
+	        }
 
-		// Only Space Member Can Invite
-		String sql = "SELECT * FROM space_members WHERE user_no = ? AND space_key = ?";
-		pstmt = conn.prepareStatement(sql);
-		pstmt.setInt(1, currentUserNo);
-		pstmt.setString(2, spaceKey);
-		rs = pstmt.executeQuery();
-
-		
-		
-		if (rs.next()) {
-			// Create Invite Code
-			String inviteCode = RandomCodeUtil.generateRandomCode();
-
-			sql = " INSERT INTO space_members(space_key, user_no, user_role, invite_code, expire_date) "
-					+ "VALUES (?, ?, ?, ?, SYSDATE + (1/24/60 * 10))";
-			pstmt2 = conn.prepareStatement(sql);
-			pstmt2.setString(1, spaceKey);
-			pstmt2.setInt(2, userNo);
-			pstmt2.setString(3, userRole);
-			pstmt2.setString(4, inviteCode);
-			pstmt2.executeUpdate();
-
-		}
-
-		rs.close();
-		pstmt2.close();
-		pstmt.close();
-		conn.close();
-
+	        // Insert after Authorize
+	        try (PreparedStatement insertPstmt = conn.prepareStatement(insertSql)) {
+	            insertPstmt.setString(1, spaceKey);
+	            insertPstmt.setInt(2, userNo);
+	            insertPstmt.setString(3, userRole);
+	            insertPstmt.setString(4, RandomCodeUtil.generateRandomCode());
+	            insertPstmt.executeUpdate();
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
 	}
 
 	// Check Invite Code
@@ -87,45 +71,39 @@ public class SpaceMembersDao {
 	// output : boolean
 	// userNo : invited user
 	boolean checkInviteCode(String inviteCode, String spaceKey, int userNo) throws Exception {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		PreparedStatement pstmt2 = null;
-		ResultSet rs = null;
 		boolean check = false;
-
-		Class.forName(driver);
-		conn = DriverManager.getConnection(url, dbId, dbPw);
-
-		// Check Invite Code
-		String sql = "SELECT * " + "FROM space_members " + "WHERE invite_code = ? "
+		String sql1 = "SELECT * " + "FROM space_members " + "WHERE invite_code = ? "
 				+ "AND expire_date > SYSDATE AND user_no = ? AND space_key = ?";
-		pstmt = conn.prepareStatement(sql);
-		pstmt.setString(1, inviteCode);
-		pstmt.setInt(2, userNo);
-		pstmt.setString(3, spaceKey);
-		rs = pstmt.executeQuery();
-
-		if (rs.next()) {
-			check = true;
-			sql = "UPDATE space_members " + "SET invite_code = null, expire_date = null " + "WHERE user_no = ? "
-					+ "AND space_key = ?";
-			pstmt2 = conn.prepareStatement(sql);
-			pstmt2.setInt(1, userNo);
-			pstmt2.setString(2, spaceKey);
-			pstmt2.executeUpdate();
-		} else {
-			sql = "DELETE FROM space_members " + "WHERE user_no = ? AND space_key = ?";
-			pstmt2 = conn.prepareStatement(sql);
-			pstmt2.setInt(1, userNo);
-			pstmt2.setString(2, spaceKey);
-			pstmt2.executeUpdate();
-			pstmt2.close();
+		String sql2 = "UPDATE space_members " + "SET invite_code = null, expire_date = null " + "WHERE user_no = ? "
+				+ "AND space_key = ?";
+		String sql3 = "DELETE FROM space_members " + "WHERE user_no = ? AND space_key = ?";
+		
+		try (Connection conn = DriverManager.getConnection(url, dbId, dbPw);
+			PreparedStatement pstmt1 = conn.prepareStatement(sql1);
+			PreparedStatement pstmt2 = conn.prepareStatement(sql2);
+			PreparedStatement pstmt3 = conn.prepareStatement(sql3)) {
+			
+			// Check Invite Code
+			pstmt1.setString(1, inviteCode);
+			pstmt1.setInt(2, userNo);
+			pstmt1.setString(3, spaceKey);
+			try (ResultSet rs = pstmt1.executeQuery()) {
+				
+				if (rs.next()) {
+					check = true;
+					pstmt2.setInt(1, userNo);
+					pstmt2.setString(2, spaceKey);
+					pstmt2.executeUpdate();
+					}
+			else {
+					pstmt3.setInt(1, userNo);
+					pstmt3.setString(2, spaceKey);
+					pstmt3.executeUpdate();
+				}
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
-
-		rs.close();
-		pstmt.close();
-		conn.close();
-
 		return check;
 	}
 
@@ -133,28 +111,21 @@ public class SpaceMembersDao {
 	// input : space_key, user_no
 	// output : user_role
 	String searchUserRole(String spaceKey, int userNo) throws Exception {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
 		String userRole = null;
-
-		Class.forName(driver);
-		conn = DriverManager.getConnection(url, dbId, dbPw);
-
 		String sql = "SELECT user_role " + "FROM space_members " + "WHERE space_key = ? AND user_no = ?";
-		pstmt = conn.prepareStatement(sql);
-		pstmt.setString(1, spaceKey);
-		pstmt.setInt(2, userNo);
-		rs = pstmt.executeQuery();
-
-		if (rs.next()) {
-			userRole = rs.getString("user_role");
+		
+		try (Connection conn = DriverManager.getConnection(url, dbId, dbPw);
+			PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pstmt.setString(1, spaceKey);
+			pstmt.setInt(2, userNo);
+			try (ResultSet rs = pstmt.executeQuery()) {
+				if (rs.next()) {
+					userRole = rs.getString("user_role");
+				}
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
-
-		rs.close();
-		pstmt.close();
-		conn.close();
-
 		return userRole;
 	}
 
@@ -162,39 +133,33 @@ public class SpaceMembersDao {
 	// input : space_key, user_no
 	// output : boolean
 	boolean deleteSpaceMember(String spaceKey, int userNo) throws Exception {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		boolean deleted = false;
-
-		Class.forName(driver);
-		conn = DriverManager.getConnection(url, dbId, dbPw);
-
-		String sql = "UPDATE task SET worker_no = null WHERE worker_no = ? AND space_key = ?";
-		pstmt = conn.prepareStatement(sql);
-		pstmt.setInt(1, userNo);
-		pstmt.setString(2, spaceKey);
-		pstmt.executeQuery();
-
-		pstmt.close();
+		boolean isDeleted = false;
+		String sql1 = "UPDATE task SET worker_no = null WHERE worker_no = ? AND space_key = ?";
+		String sql2 = "UPDATE reply SET writer_no = null WHERE writer_no = ? AND space_key = ?";
+		String sql3 = "DELETE FROM space_members " + "WHERE user_no = ? AND space_key = ?";
 		
-		sql = "UPDATE reply SET writer_no = null WHERE writer_no = ? AND space_key = ?";
-		pstmt = conn.prepareStatement(sql);
-		pstmt.setInt(1, userNo);
-		pstmt.setString(2, spaceKey);
-		pstmt.executeQuery();
-
-		pstmt.close();
-		
-		sql = "DELETE FROM space_members " + "WHERE user_no = ? AND space_key = ?";
-		pstmt = conn.prepareStatement(sql);
-		pstmt.setInt(1, userNo);
-		pstmt.setString(2, spaceKey);
-		pstmt.executeQuery();
-		
-		pstmt.close();
-		conn.close();
-
-		return true;
+		try (Connection conn = DriverManager.getConnection(url, dbId, dbPw);
+			PreparedStatement pstmt1 = conn.prepareStatement(sql1);
+			PreparedStatement pstmt2 = conn.prepareStatement(sql2);
+			PreparedStatement pstmt3 = conn.prepareStatement(sql3)) {
+			
+			pstmt1.setInt(1, userNo);
+			pstmt1.setString(2, spaceKey);
+			pstmt1.executeUpdate();
+			
+			pstmt2.setInt(1, userNo);
+			pstmt2.setString(2, spaceKey);
+			pstmt2.executeUpdate();
+	
+			pstmt3.setInt(1, userNo);
+			pstmt3.setString(2, spaceKey);
+			pstmt3.executeUpdate();
+			
+			isDeleted = true;
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return isDeleted;
 	}
 
 	// 멍세 3.4
@@ -204,13 +169,6 @@ public class SpaceMembersDao {
 	// 접속중인 유저와 같은 스페이스에 있는 모든 유저 출력
 	List<UserProfileDto> getAllSpaceMembers(int currentUserNo) throws Exception{
 		List<UserProfileDto> userList = new ArrayList<>();
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		
-		Class.forName(driver);
-		conn = DriverManager.getConnection(url, dbId, dbPw);
-		
 		String sql = "SELECT "
 				+ "    u.user_no, "
 				+ "    u.user_name, "
@@ -229,24 +187,24 @@ public class SpaceMembersDao {
 				+ "    ) "
 				+ "    AND space.user_no != ? "
 				+ ")";
-		pstmt = conn.prepareStatement(sql);
-		pstmt.setInt(1, currentUserNo);
-		pstmt.setInt(2, currentUserNo);
-		rs = pstmt.executeQuery();
-		
-		while(rs.next()) {
-			UserProfileDto dto = new UserProfileDto();
-			dto.setUserNo(rs.getInt("user_no"));
-			dto.setUserName(rs.getString("user_name"));
-			dto.setEmail(rs.getString("email"));
-			dto.setImageTitle(rs.getString("image_title"));
-			userList.add(dto);
+		try (Connection conn = DriverManager.getConnection(url, dbId, dbPw);
+			PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			
+			pstmt.setInt(1, currentUserNo);
+			pstmt.setInt(2, currentUserNo);
+			try (ResultSet rs = pstmt.executeQuery()) {
+				while(rs.next()) {
+					UserProfileDto dto = new UserProfileDto();
+					dto.setUserNo(rs.getInt("user_no"));
+					dto.setUserName(rs.getString("user_name"));
+					dto.setEmail(rs.getString("email"));
+					dto.setImageTitle(rs.getString("image_title"));
+					userList.add(dto);
+				}
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
-		
-		rs.close();
-		pstmt.close();
-		conn.close();
-		
 		return userList;
 	}
 	
@@ -257,33 +215,28 @@ public class SpaceMembersDao {
 	// 접속중인 스페이스의 모든 유저 출력
 	List<UserProfileDto> getSpaceMemberList(String currentSpaceKey) throws Exception {
 		List<UserProfileDto> memberList = new ArrayList<>();
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-
-		Class.forName(driver);
-		conn = DriverManager.getConnection(url, dbId, dbPw);
-
 		String sql = " SELECT i.image_title, u.user_no, u.user_name, u.email " + "FROM space_members s "
 				+ "JOIN users u " + "ON s.user_no = u.user_no " + "LEFT OUTER JOIN image i "
 				+ "ON u.image_no = i.image_no " + "WHERE s.space_key = ?";
-		pstmt = conn.prepareStatement(sql);
-		pstmt.setString(1, currentSpaceKey);
-		rs = pstmt.executeQuery();
+		try (Connection conn = DriverManager.getConnection(url, dbId, dbPw);
+			PreparedStatement pstmt = conn.prepareStatement(sql)){
+			
+			pstmt.setString(1, currentSpaceKey);
+			
+			try (ResultSet rs = pstmt.executeQuery()) {
 
-		while (rs.next()) {
-			UserProfileDto dto = new UserProfileDto();
-			dto.setUserNo(rs.getInt("user_no"));
-			dto.setUserName(rs.getString("user_name"));
-			dto.setEmail(rs.getString("email"));
-			dto.setImageTitle(rs.getString("image_title"));
-			memberList.add(dto);
+				while (rs.next()) {
+					UserProfileDto dto = new UserProfileDto();
+					dto.setUserNo(rs.getInt("user_no"));
+					dto.setUserName(rs.getString("user_name"));
+					dto.setEmail(rs.getString("email"));
+					dto.setImageTitle(rs.getString("image_title"));
+					memberList.add(dto);
+				}
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
-
-		rs.close();
-		pstmt.close();
-		conn.close();
-
 		return memberList;
 	}
 
