9ca5ee04da21c0beaa9a2d1f0619aa782c26603b
KIMSUNGKWAN <khska44@gmail.com>
2026-06-08 17:22:26 +0900

    sdsds

--- src/alarnchk/AlarmChkDao.java
+++ src/alarnchk/AlarmChkDao.java
@@ -92,6 +92,7 @@ public class AlarmChkDao {
 
 	public static void main(String[] args) {
 		AlarmChkDao dao = new AlarmChkDao();
+		
 		// 10.1 Create Alarm Check(Clear!)
 		System.out.println(dao.CreateAlarmCheck(1, 1));
 		// 10.2 Delete Alarm Check(Clear!)
