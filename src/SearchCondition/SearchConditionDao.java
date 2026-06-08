6ae4170723cf3af8319964eefdc47a2feba9197a
Minjae Seo <seomj081923@gmail.com>
2026-06-08 16:45:36 +0900

    Update SearchConditionDao

--- src/SearchCondition/SearchConditionDao.java
+++ src/SearchCondition/SearchConditionDao.java
@@ -3,7 +3,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchConditionDao {
 	String driver = "oracle.jdbc.driver.OracleDriver";
@@ -105,6 +106,7 @@ public class SearchConditionDao {
 			if(priority != null && !priority.isBlank()) {
 				pstmt6.setString(1, priority);
 				chk += pstmt6.executeUpdate();
+				cnt++;
 			}
 			
 			if(statusNo != null) {
@@ -146,31 +148,192 @@ public class SearchConditionDao {
 		return isCreated;
 	}
 	
-	// 명세 8.2
-	// Input : 
-	// Output : boolean
-	boolean updateSearchCondition(
-			String searchConditionTitle, 
-			Integer workerNo, 
-			Integer creatorNo, 
-			Integer statusNo, 
-			String priority, 
-			String labelTitle, 
-			String spaceKey, 
-			String dueDate, 
-			String operatorDueDate
-			) throws Exception{
-		int chk = 0;
-		boolean isUpdated = false;
-		String sql = "UPDATE ";
-		
+	// Import User Roll for Search Condition
+	String importSearchConditionAccessType (int searchConditionNo, int currentUserNo) throws Exception {
+		String accessType = "";
+		String sql = "SELECT access_type FROM search_condition_access WHERE search_condition_no = ? AND user_no = ?";
 		try (Connection conn = DriverManager.getConnection(url, dbId, dbPw);
 				PreparedStatement pstmt = conn.prepareStatement(sql)) {
-		
+			
+			pstmt.setInt(1, searchConditionNo);
+			pstmt.setInt(2, currentUserNo);
+			
+			try (ResultSet rs = pstmt.executeQuery()) {
+				if (rs.next()) {
+					accessType = rs.getString("access_type");
+				}
+			}
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		return isUpdated;
+		
+		return accessType;
+	}
+	
+	// 명세 8.2
+	// Input : currentUserNo, user_no, access_type, space_key, search_condition_no
+	// Output : boolean
+	// Create Access Type in Search Condition
+	boolean createSearchConditionAccessType (int searchConditionNo, int userNo, String spaceKey, String accessType) throws Exception {
+		boolean isCreated = false;
+		String sql = "INSERT INTO search_condition_access (search_condition_no, user_no, space_key, access_type) "
+				+ "VALUES (?, ?, ?, ?)";
+		
+		try (Connection conn = DriverManager.getConnection(url, dbId, dbPw);
+				PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pstmt.setInt(1, searchConditionNo);
+			pstmt.setInt(2, userNo);
+			pstmt.setString(3, spaceKey);
+			pstmt.setString(4, accessType);
+			
+			if(pstmt.executeUpdate() != 0) {
+				isCreated = true;
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return isCreated;
+	}
+	
+	// Delete Access Type in SearchCondition
+	boolean deleteSearchConditionAccessType (int searchConditionNo, int userNo, String currentUserAccessType) throws Exception {
+		boolean isDeleted = false;
+		String sql = "DELETE FROM search_condition_access WHERE search_condition_no = ? AND user_no = ? ";
+		
+		try (Connection conn = DriverManager.getConnection(url, dbId, dbPw);
+				PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			if("owner".equals(currentUserAccessType) || "editor".equals(currentUserAccessType)) {
+				pstmt.setInt(1, searchConditionNo);
+				pstmt.setInt(2, userNo);
+			}
+			if(pstmt.executeUpdate() != 0) {
+				isDeleted = true;
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return isDeleted;
+	}
+	
+	// 명세 8.3
+	// Input : search_condition_no, current_user_no, access_type
+	// Output : boolean
+	boolean deleteSearchCondition(int searchConditionNo, int currentUserNo, String accessType) throws Exception {
+		boolean isDeleted = false;
+		String sql = "DELETE FROM search_condition WHERE search_condition_no = ?";
+		
+		try (Connection conn = DriverManager.getConnection(url, dbId, dbPw);
+				PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			
+			if("owner".equals(accessType) || "editor".equals(accessType)) {
+				pstmt.setInt(1, searchConditionNo);
+			}
+			else {
+				return false;
+			}
+			if (pstmt.executeUpdate() != 0) {
+				isDeleted = true;
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return isDeleted;
+	}
+	
+	// 명세 8.4
+	// Input : current_user_no, access_type
+	// Output : 
+	List<SearchConditionDto> showSearchCondition (int currentUserNo, String accessType) throws Exception {
+		List<SearchConditionDto> searchConditionList = new ArrayList<>();
+		ArrayList<Integer> searchConditionNo = new ArrayList<>();
+		
+		String sql = "SELECT search_condition_no FROM search_condition_access WHERE user_no = ? AND access_type = 'owner'";
+		String titleSql = "SELECT search_condition_title FROM search_condition WHERE search_condition_no = ?";
+		String workerSql = "SELECT worker_no FROM search_condition_detail_worker WHERE search_condition_no = ?";
+		String creatorSql = "SELECT creator_no FROM search_condition_detail_creator WHERE search_condition_no = ?";
+		String statusSql = "SELECT status_no FROM search_condition_detail_status WHERE search_condition_no = ?";
+		String prioritySql = "SELECT priority FROM search_condition_detail_priority WHERE search_condition_no = ?";
+		String spaceKeySql = "SELECT space_key FROM search_condition_detail_space WHERE search_condition_no = ?";
+		String dueDateSql = "SELECT due_date FROM search_condition_detail_due_date WHERE search_condition_no = ?";
+		
+		try (Connection conn = DriverManager.getConnection(url, dbId, dbPw);
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				PreparedStatement pstmtTitle = conn.prepareStatement(titleSql);
+				PreparedStatement pstmtWorker = conn.prepareStatement(workerSql);
+				PreparedStatement pstmtCreator = conn.prepareStatement(creatorSql);
+				PreparedStatement pstmtStatus = conn.prepareStatement(statusSql);
+				PreparedStatement pstmtPriority = conn.prepareStatement(prioritySql);
+				PreparedStatement pstmtSpaceKey = conn.prepareStatement(spaceKeySql);
+				PreparedStatement pstmtDueDate = conn.prepareStatement(dueDateSql)) {
+			
+			// 유저가 owner인 searchConditionNo들을 조회
+			pstmt.setInt(1, currentUserNo);
+			try (ResultSet rs = pstmt.executeQuery()){
+				while(rs.next()) {
+					searchConditionNo.add(rs.getInt("search_condition_no"));
+				}
+			}
+			
+			// 조회된 searchConditionNo들의 정보들을 취합
+			for (int n : searchConditionNo) {
+				
+				pstmtTitle.setInt(1, n);
+				try (ResultSet rsTitle = pstmtTitle.executeQuery()) {
+					if (rsTitle.next()) {
+						SearchConditionDto dto = new SearchConditionDto();
+						dto.setSearchConditionTitle(rsTitle.getString("search_condition_title"));
+						
+						pstmtWorker.setInt(1, n);
+						try (ResultSet rsWorker = pstmtWorker.executeQuery()) {
+							if (rsWorker.next()) {
+								dto.setWorkerNo(rsWorker.getInt("worker_no"));
+							}
+						}
+						
+						pstmtCreator.setInt(1, n);
+						try (ResultSet rsCreator = pstmtCreator.executeQuery()) {
+							if (rsCreator.next()) {
+								dto.setCreatorNo(rsCreator.getInt("creator_no"));
+							}
+						}
+						
+						pstmtStatus.setInt(1, n);
+						try (ResultSet rsStatus = pstmtStatus.executeQuery()) {
+							if (rsStatus.next()) {
+								dto.setStatusNo(rsStatus.getInt("status_no"));
+							}
+						}
+						
+						pstmtPriority.setInt(1, n);
+						try (ResultSet rsPriority = pstmtPriority.executeQuery()) {
+							if (rsPriority.next()) {
+								dto.setPriority(rsPriority.getString("priority"));
+							}
+						}
+						
+						pstmtSpaceKey.setInt(1, n);
+						try (ResultSet rsSpaceKey = pstmtSpaceKey.executeQuery()) {
+							if (rsSpaceKey.next()) {
+								dto.setSpaceKey(rsSpaceKey.getString("space_key"));
+							}
+						}
+						
+						pstmtDueDate.setInt(1, n);
+						try (ResultSet rsDueDate = pstmtDueDate.executeQuery()) {
+							if (rsDueDate.next()) {
+								dto.setDueDate(rsDueDate.getString("due_date"));
+							}
+						}
+						searchConditionList.add(dto);
+					}
+					
+				}
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return searchConditionList;
 	}
 	
 	public static void main(String[] args) throws Exception {
